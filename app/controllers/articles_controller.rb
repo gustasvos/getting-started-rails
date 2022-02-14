3c8b5fe44c51a66509d8d2bd5ddbd25ab169d4d6
Gustavo Rosa <gustavorib.rosa@gmail.com>
2022-02-14 14:18:25 -0300

    initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,26 @@
+class ArticlesController < ApplicationController
+  # each method must have a view to render.
+  # def index method will render 'app/views/articles/index.html.erb' and so on.
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+end
